6c891e1ab62494934c4ed34394ecd9da3eda57a9
Angelica Garcia <anyegar36@gmail.com>
2023-01-02 11:49:09 -0500

    Add message welcome app

---
+++ src/welcome.js
@@ -0,0 +1,28 @@
+const figlet = require('figlet');
+const chalk = require('chalk');
+
+const log = console.log;
+
+const welcome = () => {
+  log(
+    chalk.keyword('orange')(
+      figlet.textSync('< md_links >', { horizontalLayout: 'full' }),
+    ),
+  );
+  log('');
+  log(chalk.bold.blue('.:.:.:. Herramienta para validar links de archivos .md .:.:.:.'));
+  log();
+  log('');
+  log(chalk.bgRed('Para comenzar a usar md-links, ten en cuenta las siguientes instrucciones:'));
+  log('');
+  log(chalk.green('1. Ingresa el comando md-links seguido de la ruta del archivo que deseas analizar.'));
+  log('');
+  log(chalk.green('2. Si deseas validar los links, agrega la opción --validate.'));
+  log('');
+  log(chalk.green('3. Si deseas obtener estadísticas de los links, agrega la opción --stats.'));
+  log('');
+  log(chalk.green('4. Si deseas obtener estadísticas de los links y validarlos, agrega las opciones --stats y --validate.'));
+  log('');
+};
+
+module.exports = welcome;
